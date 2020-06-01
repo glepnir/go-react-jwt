da7d0fa235959f8636f8778a5669521da81e0ce4
glepnir <glepnir@gopherhub.org>
2020-06-01 23:09:09 +0800

    add Loading compoent

--- client/src/routers/index.tsx
+++ client/src/routers/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
-import Login from '../views/login';
 import Home from '../views/home';
+import Loading from '../components/Loading';
+
+const Login = React.lazy(() => import('./../views/login'));
 
 const Routers: React.FC = () => (
   <HashRouter>
-    <Switch>
-      <Route exact path="/" component={Login} />
-      <Route path="/home" component={Home} />
-    </Switch>
+    <Suspense fallback={<Loading />}>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/home" component={Home} />
+      </Switch>
+    </Suspense>
   </HashRouter>
 );
 
