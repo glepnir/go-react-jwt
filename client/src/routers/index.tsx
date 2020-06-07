029f958342f9deea17406ca9d6ead0376e00daad
Raphael <sawakitaeiji233@gmail.com>
2020-06-07 22:03:27 +0800

    update

--- client/src/routers/index.tsx
+++ client/src/routers/index.tsx
@@ -3,14 +3,14 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 import Loading from '../components/Loading';
 import routes from './routes';
 
-const routeComponents = routes.map(({ path, exact, component }) => (
-  <Route exact={exact} path={path} component={component} />
+const renderRoutes = routes.map(({ path, exact, component }) => (
+  <Route exact={exact || false} path={path} component={component} />
 ));
 
 const Routers: React.FC = () => (
   <HashRouter>
     <Suspense fallback={<Loading />}>
-      <Switch>{routeComponents}</Switch>
+      <Switch>{renderRoutes}</Switch>
     </Suspense>
   </HashRouter>
 );
