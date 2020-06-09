f124e5e6c22dc7c4c5f874921d2c58e03d4e45aa
Raphael <sawakitaeiji233@gmail.com>
2020-06-09 17:26:32 +0800

    add key on routes

--- client/src/routers/index.tsx
+++ client/src/routers/index.tsx
@@ -3,8 +3,8 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 import Loading from '../components/Loading';
 import routes from './routes';
 
-const renderRoutes = routes.map(({ path, exact, component }) => (
-  <Route exact={exact || false} path={path} component={component} />
+const renderRoutes = routes.map(({ path, exact, component, key }) => (
+  <Route exact={exact || false} path={path} key={key} component={component} />
 ));
 
 const Routers: React.FC = () => (
