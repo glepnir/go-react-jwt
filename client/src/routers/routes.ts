3edd680e55cb9485769ce587a5af3e3d09d73e71
glepnir <glepnir@gopherhub.org>
2020-06-09 17:26:32 +0800

    add key on routes

--- client/src/routers/routes.ts
+++ client/src/routers/routes.ts
@@ -3,11 +3,13 @@ import { lazy } from 'react';
 const routes = [
   {
     path: '/',
+    key: 'login',
     exact: true,
     component: lazy(() => import('./../views/login')),
   },
   {
     path: '/home',
+    key: 'home',
     component: lazy(() => import('./../views/home')),
   },
 ];
